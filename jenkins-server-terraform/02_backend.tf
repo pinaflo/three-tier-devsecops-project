04c6d0088aac58f238155f3c2aacd27083e6d907
pinaflo <102924052+pinaflo@users.noreply.github.com>
2025-02-03 07:16:01 +0000

    Update 02_backend.tf

--- jenkins-server-terraform/02_backend.tf
+++ jenkins-server-terraform/02_backend.tf
@@ -2,10 +2,10 @@
 terraform {
   backend "s3" {
     # The S3 bucket where the Terraform state file will be stored.
-    bucket         = "three-tier-devsecops-project-bucket-s3"
+    bucket         = "pinaflo-bucket"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
-    region         = "us-west-2"
+    region         = "us-east-1"
     
     # The key (path) within the S3 bucket for storing the state file.
     key            = "three-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
